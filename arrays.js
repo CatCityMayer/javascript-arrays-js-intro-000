dfeafc65fb590299cbfc047dde6235c06cc4cbfd
CatCityMayor <chadtroy83@gmail.com>
2018-12-05 21:53:10 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,13 @@
+var chocolateBars = [
+  "snickers", "hundred grand", "kitkat", "skittles"
+]
+
+function addElementToBeginningOfArray([1]. "fish") {
+  var myArray["shark", "dolphin"];
+  myArray["fish", ...myArray]
+}
+
+function destructivelyAddElementToBeginningOfArray(){
+  var myArray["shark", "dolphin"];
+  myArray[0] = "Fish"
+}
