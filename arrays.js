e506c23416fa483365eb58315d3ac064e4ec040c
CatCityMayor <chadtroy83@gmail.com>
2018-12-05 22:11:13 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -2,12 +2,12 @@ var chocolateBars = [
   "snickers", "hundred grand", "kitkat", "skittles"
 ]
 
-function addElementToBeginningOfArray(element, [array]) {
+function addElementToBeginningOfArray(element, array) {
   ["element", ...array];
   return array
 }
 
-function destructivelyAddElementToBeginningOfArray(element, [array]){
+function destructivelyAddElementToBeginningOfArray(element, array){
   array.unshift("element");
   return array
 }
