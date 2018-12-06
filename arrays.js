19a2b79a312da24c9344cc041ab098e880d4fd6f
CatCityMayor <chadtroy83@gmail.com>
2018-12-06 22:28:39 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -2,6 +2,7 @@ var chocolateBars = [
   "snickers", "hundred grand", "kitkat", "skittles"
 ]
 
+
 function addElementToBeginningOfArray(array, element) {
   ["element", ...array];
   return array
@@ -10,4 +11,14 @@ function addElementToBeginningOfArray(array, element) {
 function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift("element");
   return array
+
+function addElementToBeginningOfArray([1]. "fish") {
+  var myArray["shark", "dolphin"];
+  myArray["fish", ...myArray]
+}
+
+function destructivelyAddElementToBeginningOfArray(){
+  var myArray["shark", "dolphin"];
+  myArray[0] = "Fish"
+
 }
